41f51e35c87ea16ed06943142ecdf6880932d628
fasanda4 <fasanda04@gmail.com>
2024-11-15 09:45:32 +0300

    Add files via upload

---
+++ conct1/conct1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "Работа";
+        char dash = '–';
+        string str2 = "не волк, в лес не убежит, потому её, окаянную, делать и надо.";
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("Выводимое константное сообщение...");
+
+        Console.WriteLine(str1 + " " + dash + " " + str2);
+        Console.WriteLine("Нажмите любую клавишу, чтобы закрыть окно...");
+        Console.ReadKey(true);
+    }
+}
+
