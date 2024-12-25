d7dc3bca86d17811e63d248bdb2e161fa32823da
fasanda4 <fasanda04@gmail.com>
2024-12-26 02:56:53 +0300

    Add files via upload

--- zad_9/zad_9/Program.cs
+++ zad_9/zad_9/Program.cs
@@ -4,24 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Блок объявления всех переменных
+        int n = 0; // Длина массива
+        int[] array = null; // Массив целых чисел
+        bool flg = false; // Флаг для проверки корректности ввода
+        int positiveIndex = 0; // Индекс для положительных элементов
+        int negativeIndex = 0; // Индекс для отрицательных элементов
+        int temp = 0; // Переменная для обмена значений
+
         // Запрашиваем у пользователя длину массива
         Console.Write("Введите длину массива: ");
+
         // Пытаемся преобразовать введенную строку в целое число
-        // Если преобразование успешно, результат сохраняется в переменной n, а flg принимает значение true
-        bool flg = Int32.TryParse(Console.ReadLine(), out int n);
+        flg = Int32.TryParse(Console.ReadLine(), out n);
 
         // Проверяем, удалось ли корректно преобразовать введенные данные
         if (flg == true)
         {
             // Создаем массив целых чисел длиной n
-            int[] array = new int[n];
+            array = new int[n];
 
             // Цикл для ввода элементов массива пользователем
             for (int i = 0; i < array.Length; i++)
             {
                 // Просим пользователя ввести i-й элемент массива
                 Console.Write($"Введите {i + 1}-й элемент массива: ");
-                // Пытаемся преобразовать введенную строку в целое число и записать в массив
                 flg = Int32.TryParse(Console.ReadLine(), out array[i]);
 
                 // Если введенные данные некорректны, выходим из цикла
@@ -34,17 +41,15 @@ class Program
             // Проверяем, успешно ли были введены все элементы массива
             if (flg == true)
             {
-                // Инициализируем индекс для поиска положительных чисел
-                int positiveIndex = 0;
-
                 // Находим первый отрицательный элемент в массиве
+                positiveIndex = 0;
                 while (positiveIndex < array.Length && array[positiveIndex] >= 0)
                 {
                     positiveIndex++; // Перемещаемся к следующему элементу, пока он не станет отрицательным
                 }
 
                 // Устанавливаем индекс для поиска отрицательных элементов
-                int negativeIndex = positiveIndex;
+                negativeIndex = positiveIndex;
 
                 // Находим следующий положительный элемент после отрицательного
                 while (negativeIndex < array.Length && array[negativeIndex] < 0)
@@ -56,7 +61,7 @@ class Program
                 while (positiveIndex < array.Length && negativeIndex < array.Length)
                 {
                     // Обмен значениями между положительным и отрицательным элементами
-                    int temp = array[positiveIndex];
+                    temp = array[positiveIndex];
                     array[positiveIndex] = array[negativeIndex];
                     array[negativeIndex] = temp;
 
